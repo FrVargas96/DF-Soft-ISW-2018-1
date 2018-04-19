19d007ac24d6115f8e337022b14a9f0022f90741
fvargasf <francis.vargas@sansano.usm.cl>
2018-04-19 17:04:07 -0400

    Programa base

---
+++ Proyecto ISW1/Proyecto ISW/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Proyecto_ISW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.ingesar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.calcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.ValorF = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ingesar
+            // 
+            this.ingesar.BackColor = System.Drawing.Color.Transparent;
+            this.ingesar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ingesar.Location = new System.Drawing.Point(13, 17);
+            this.ingesar.Name = "ingesar";
+            this.ingesar.Size = new System.Drawing.Size(116, 23);
+            this.ingesar.TabIndex = 0;
+            this.ingesar.Text = "Ingresar CSV";
+            this.ingesar.UseVisualStyleBackColor = false;
+            this.ingesar.Click += new System.EventHandler(this.ingresar_archivo);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.calcular);
+            this.panel1.Controls.Add(this.ingesar);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(32, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(148, 447);
+            this.panel1.TabIndex = 1;
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(13, 46);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(116, 23);
+            this.calcular.TabIndex = 1;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_valor);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Rockwell Extra Bold", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DF Software";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel2.Controls.Add(this.ValorF);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(200, 89);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(643, 71);
+            this.panel2.TabIndex = 3;
+            // 
+            // ValorF
+            // 
+            this.ValorF.AutoSize = true;
+            this.ValorF.Enabled = false;
+            this.ValorF.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ValorF.Location = new System.Drawing.Point(107, 27);
+            this.ValorF.Name = "ValorF";
+            this.ValorF.Size = new System.Drawing.Size(107, 25);
+            this.ValorF.TabIndex = 1;
+            this.ValorF.Text = "Esperando";
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 35);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Valor: ";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MediumPurple;
+            this.panel3.Controls.Add(this.chart1);
+            this.panel3.Location = new System.Drawing.Point(200, 181);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(643, 355);
+            this.panel3.TabIndex = 4;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(24, 33);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(601, 288);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(908, 548);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ingesar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label ValorF;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
